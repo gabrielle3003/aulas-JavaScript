a64f77f33610f7eadaff3c84f8861377d5f1cd8c
Gabrielle Santos <gabrielle_santos151@estudante.sesisenai.org.br>
2026-09-14 22:26:37 -0300

    Adicao dos exemplos 04 até o 07

---
+++ exemplo07-2.js
@@ -0,0 +1,12 @@
+let nota = 85;
+
+switch (true) {
+  case nota >= 90:
+    console.log('Conceito A');
+    break;
+  case nota >= 80:
+    console.log('Conceito B');
+    break;
+  default:
+    console.log('Precisa melhorar');
+}
